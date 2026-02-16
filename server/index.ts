6b3361e062b0e5e109ee5f0b70afd61e7aa7eded
Abu Bakkar Siddique <abubakkar2502@gmail.com>
2026-02-17 00:49:59 +0530

    fix: server host issue

--- server/index.ts
+++ server/index.ts
@@ -90,8 +90,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-  const host = process.env.HOST || "127.0.0.1";
-  httpServer.listen(port, host, () => {
-    log(`serving on ${host}:${port}`);
-  });
+  httpServer.listen(port, () => {
+    log(`serving on port ${port}`);
+  });  
 })();
